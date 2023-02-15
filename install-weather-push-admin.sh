9f68ca4304de738c8e3e1074072978cdb9c664cf
yangh124 <yh.124@qq.com>
2023-02-15 21:08:15 +0800

    .

---
+++ install-weather-push-admin.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+if [ -z "$1" ];then
+    echo "请输入image.repository！"
+    exit 0
+fi
+if [ -z "$2" ];then
+    echo "请输入image.tag！"
+    exit 0
+fi
+helm upgrade --install weather-push-admin /root/mydata/cicd/weather-push-admin --set image.repository="$1" --set image.tag="$2"
