d3c685cab49ec31022fc1b29e31f09fd5590cabd
yangh124 <yh.124@qq.com>
2023-02-15 21:02:13 +0800

    .

--- install-weather-push.sh
+++ install-weather-push.sh
@@ -1,6 +1,18 @@
 #/bin/bash
 if [ -z "$1" ];then
-    echo "请输入镜像版本！"
+    echo "请输入image.repository！"
     exit 0
 fi
-helm upgrade --install weather-push /root/mydata/cicd/weather-push --set image.tag="$1"
+if [ -z "$2" ];then
+    echo "请输入image.tag！"
+    exit 0
+fi
+if [ -z "$3" ];then
+    echo "请输入service.type！"
+    exit 0
+fi
+if [ -z "$4" ];then
+    echo "请输入service.port！"
+    exit 0
+fi
+helm upgrade --install weather-push /root/mydata/cicd/weather-push --set image.repository！="$1" --set image.tag！="$2" --set service.type="$3" --set service.port！="$4"
