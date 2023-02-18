3fe8a6f1309656e3ffe9e1d2536a6f24d6330ebc
yangh124 <yh.124@qq.com>
2023-02-18 10:29:46 +0800

    .

--- install-weather-push.sh
+++ install-weather-push.sh
@@ -7,12 +7,4 @@ if [ -z "$2" ];then
     echo "请输入image.tag！"
     exit 0
 fi
-if [ -z "$3" ];then
-    echo "请输入service.type！"
-    exit 0
-fi
-if [ -z "$4" ];then
-    echo "请输入service.port！"
-    exit 0
-fi
 helm upgrade --install weather-push /root/mydata/cicd/weather-push --set image.repository="$1" --set image.tag="$2"
