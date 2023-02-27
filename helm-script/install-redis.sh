feb957e735b2bdc08053cadb15ac1c92577f110f
yangh124 <yh.124@qq.com>
2023-02-27 20:50:16 +0800

    修改配置

--- helm-script/install-redis.sh
+++ helm-script/install-redis.sh
@@ -4,7 +4,6 @@
 #--set master.service.nodePorts.redis=30919 
 #--set global.redis.password=root
 #--set primary.persistence.storageClass="local-storage-mysql"
-#--set primary.persistence.storageClass="local-storage-mysql"
 #--set primary.persistence.size=1Gi
 #--set primary.persistence.existingClaim="mysql-pvc"
 helm upgrade --install redis /root/mydata/cicd/charts/redis --namespace db-system
